6d1daddc4abbd60996fa79be3d0138aa6a069abc
Khalid Hersafril <khalidowlwalid@gmail.com>
2021-10-15 23:32:14 +0800

    Fixes Particle's motion

--- particle_simulation.py
+++ particle_simulation.py
@@ -8,11 +8,13 @@ class InitialState:
         # Read google docs for naming convention
         self.sub_1 = []
         self.sub_2 = []
-        self.Np = 5000
+        self.Np = 1000
         self.h = 0.1
         self.time = 0.0
         self.tEnd = 0.4
         self.D = 0.1
+        self.lower_lim = -1
+        self.upper_lim = 1
         
         # Figure used in graph
         self.figure, self.axes = plt.subplots(nrows=2, ncols=2)
@@ -23,23 +25,30 @@ class InitialState:
         self.x = np.random.uniform(low=-1,high=1,size=self.Np)
         self.y = np.random.uniform(low=-1,high=1,size=self.Np)
 
-        self.lang = np.random.normal(loc=0, scale=1, size=self.Np)
-
         self.subplot_position = {"(0,1)":[(0,1)],
                                  "(1,0)":[(1,0)],
                                  "(1,1)":[(1,1)]
+                                 #"(2,0)":[(2,0)],
+                                 #"(2,1)":[(2,1)]
                                  }
 
         self.subplots = ["(0,1)","(1,0)","(1,1)"]
 
     # Calculate the next position of the particle using euler's method
-    def euler(self,coordinate,i):
-        return coordinate + 2*np.sqrt(2*self.D) * np.sqrt(self.h) * self.lang[i]
+    def euler(self,coordinate,i, random):
+        sqrt_d = np.sqrt(2*self.D)
+        sqrt_h = np.sqrt(self.h)
+        print("coordinate:", coordinate)
+        print("random: ", random)
+        print("sqrt_d:", sqrt_d)
+        print("sqrt_h:", sqrt_h)
+        
+        return coordinate + 2*sqrt_d * sqrt_h * random
 
     # Creates the initial state for task A
     def taskA_initial_state(self):
         
-        isInside = np.sqrt(self.x**2 + self.y**2) <= 1
+        isInside = self.x**2 + self.y**2 <= 0.3**2
 
         plt.xlim(-1,1)
         plt.ylim(-1,1)
@@ -48,13 +57,13 @@ class InitialState:
         for i in range(self.Np):
             
             # If the position of the particle is within a circle of radius 1 centred at the origin, add the particle as substance 1
-            if self.x[i] ** 2 + self.y[i]**2 <= 1:
+            if self.x[i]**2 + self.y[i]**2 <= 0.3**2:
                 self.sub_1.append((self.x[i], self.y[i]))
 
             # Vice versa
             else:
                 self.sub_2.append((self.x[i], self.y[i]))
-
+                
         # Conditional statement to assign the position of the particle in the grid
         self.axes[0,0].scatter(self.x[~isInside], self.y[~isInside],s=5, c="blue")
         self.axes[0,0].scatter(self.x[isInside], self.y[isInside],s=5, c="red")
@@ -66,9 +75,6 @@ class InitialState:
     def taskB_initial_state(self):
 
         toTheLeft = self.x <= 0
-
-        plt.xlim(-1,1)
-        plt.ylim(-1,1)
 
         # Divide the particles into their substance type
         for i in range(self.Np):
@@ -91,35 +97,82 @@ class InitialState:
 
         for i, subplot in enumerate(self.subplots):
 
+            #print("Substance 1: ", self.sub_1)
+            #print("Substance 2: ", self.sub_2)
+
             row, col = self.subplot_position[subplot][0][0],self.subplot_position[subplot][0][1]
 
             self.time += 0.1
 
             # Here, we calculate the next position of each particle in further time step, h
             for i, particle in enumerate(self.sub_1):
+
+                lang = np.random.normal(loc=0, scale=1, size=2)
                 
                 x, y = particle[0], particle[1]
-                next_pos_x = self.euler(x,i)
-                next_pos_y = self.euler(y,i)
+                euler = lambda coordinate, random : coordinate + 2 * np.sqrt(2 * self.D) * np.sqrt(self.h) * random
+                next_pos_x = euler(x, lang[0])
+                next_pos_y = euler(y, lang[1])
+
+                if next_pos_x > 1:
+                    distanceFromMax = next_pos_x - 1
+                    next_pos_x = next_pos_x - 2 * distanceFromMax
+
+                elif next_pos_x < -1:
+                    distanceFromMax = next_pos_x + 1
+                    next_pos_x = next_pos_x + 2 * distanceFromMax
+
+                if next_pos_y > 1:
+                    distanceFromMax = next_pos_y - 1
+                    next_pos_x = next_pos_y - 2 * distanceFromMax
+
+                elif next_pos_y < -1:
+                    distanceFromMax = next_pos_y + 1
+                    next_pos_x = next_pos_y + 2 * distanceFromMax
+                #next_pos_x = x + self.euler(x,i,lang[0])
+                #next_pos_y = y + self.euler(y,i, lang[1])
 
                 # Reassign the current particle from substance 1 (in this case), to a new position
                 self.sub_1[i] = (next_pos_x, next_pos_y)
-     
+
             # Same thing as above
             for i, particle in enumerate(self.sub_2):
                 
-                # Add a comment
+                lang = np.random.normal(loc=0, scale=1, size=2)
+                
                 x, y = particle[0], particle[1]
-                next_pos_x = self.euler(x,i)
-                next_pos_y = self.euler(y,i)
+                euler = lambda coordinate, random : coordinate + 2 * np.sqrt(2 * self.D) * np.sqrt(self.h) * random
+                next_pos_x = euler(x, lang[0])
+                next_pos_y =euler(y, lang[1])
+                #next_pos_x = x + self.euler(x,i,lang[0])
+                #next_pos_y = y + self.euler(y,i, lang[1])
+
+                if next_pos_x > 1:
+                    distanceFromMax = next_pos_x - 1
+                    next_pos_x = next_pos_x - 2 * distanceFromMax
+
+                elif next_pos_x < -1:
+                    distanceFromMax = next_pos_x + 1
+                    next_pos_x = next_pos_x + 2 * distanceFromMax
+
+                if next_pos_y > 1:
+                    distanceFromMax = next_pos_y - 1
+                    next_pos_x = next_pos_y - 2 * distanceFromMax
+
+                elif next_pos_y < -1:
+                    distanceFromMax = next_pos_y + 1
+                    next_pos_x = next_pos_y + 2 * distanceFromMax
 
                 self.sub_2[i] = (next_pos_x, next_pos_y)
-
-            plt.xlim(-1,1)
-            plt.ylim(-1,1)
+                
             # Plots the new coordinate of substance 1 and substance 2
             self.plot(self.sub_1, row, col)
             self.plot(self.sub_2, row, col)
+
+        out_particle = self.check_particle()
+
+        print("Inside particle: {calc}".format(calc=(self.Np - len(out_particle))))
+            
 
     def plot(self, particle_list,row,col):
 
@@ -130,17 +183,46 @@ class InitialState:
             color="blue"
 
         for i, particle in enumerate(particle_list):
-            self.axes[row,col].scatter(particle_list[i][0], particle_list[i][1], s=5, c=color)
+            self.axes[row,col].set_xbound(lower=self.lower_lim, upper=self.upper_lim)
+            self.axes[row,col].set_xlim(xmin=self.lower_lim, xmax=self.upper_lim)
+            self.axes[row,col].set_ybound(lower=self.lower_lim, upper=self.upper_lim)
+            self.axes[row,col].set_ylim(ymin=self.lower_lim, ymax=self.upper_lim)
+            self.axes[row,col].scatter(particle_list[i][0], particle_list[i][1], s=3, c=color)
 
-    def main(self):
+    def check_particle(self):
+
+        out_particle = []
+
+        for i, particle in enumerate(self.sub_1):
+            
+            x, y = particle[0], particle[1]
+
+            if x > 0 or y > 0 or x < -1 or y < -1: 
+                out_particle.append(particle)
+
+        for i, particle in enumerate(self.sub_2):
+            
+            x, y = particle[0], particle[1]
+
+            if x > 0 or y > 0 or x < -1 or y < -1: 
+                out_particle.append(particle)
+
+        return out_particle
+
+
+
+    def call_out_taskB(self):
         self.taskB_initial_state()
         self.estimate_next_position()
-        #self.estimate_next_position()
-        #self.estimate_next_position()
+        plt.show()
+
+    def call_out_taskA(self):
+        self.taskA_initial_state()
+        self.estimate_next_position()
         plt.show()
 
 state = InitialState()
-state.main()
+state.call_out_taskA()
 
 
 
