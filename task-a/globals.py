fc44920037df88b20d2047b07c4d7e495aba1630
Khalid Hersafril <khalidowlwalid@gmail.com>
2021-10-28 19:21:13 +0800

    New file added

--- task-a/globals.py
+++ task-a/globals.py
@@ -1,1 +1,10 @@
 # Define all variable here
+
+class Globals:
+
+    def __init__(self):
+        
+        self.xMax = 1
+        self.xMin = -1
+        self.yMax = 1
+        self.yMin = -1
--- task-a/simulation.py
+++ task-a/simulation.py
@@ -1,3 +1,7 @@
 # Here is where the all the backends for the simulation happens
 import numpy as np
 import matplotlib.pyplot as plt
+
+from globals import Globals
+
+
